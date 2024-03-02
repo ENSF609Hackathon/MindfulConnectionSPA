cf5156d5bb7e9c27bd57458e9b1526989f4df34b
Paul Uhrich <paul.uhrich@ucalgary.ca>
2024-03-02 12:32:26 -0700

    Fixed header and titles.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,6 +22,7 @@ import Postview from '../components/Postview';
 import Load from '../components/Load';
 import Profilephoto from '../components/Profilephoto';
 import baseImage from "../assets/loginPeople.webp";
+import logo from '../assets/Logo.jpg'
 
 const Home = () =>  {
 
@@ -66,24 +67,26 @@ const Home = () =>  {
     } else {
         return(
             <Fragment>
-                <div className="main-wrap">
-                    <div className="nav-header bg-transparent shadow-none border-0">
-                        <div className="nav-top w-100">
-                            <a href="/"><i className="feather-heart text-success display1-size me-2 ms-0"></i><span className="d-inline-block fredoka-font ls-3 fw-600 text-current font-xxl logo-text mb-0">Mindful Connection</span> </a>
-                            <button className="nav-menu me-0 ms-auto"></button>
+                {/*<div className="main-wrap">*/}
+                {/*    <div className="nav-header bg-transparent shadow-none border-0">*/}
+                {/*        <div className="nav-top w-100">*/}
+                {/*            <a href="/"><span className="d-inline-block fredoka-font ls-3 fw-600 text-current font-xxl logo-text mb-0">Mindful Connection</span> </a>*/}
+                {/*            <button className="nav-menu me-0 ms-auto"></button>*/}
 
-                            {/*<a onClick={handlesLogout} className="header-btn d-none d-lg-block bg-dark fw-500 text-white font-xsss p-3 ms-auto w100 text-center lh-20 rounded-xl">Login</a>*/}
-                            {/*<a href="/register" className="header-btn d-none d-lg-block bg-current fw-500 text-white font-xsss p-3 ms-2 w100 text-center lh-20 rounded-xl">Register</a>*/}
-                        </div>
-                    </div>
+                {/*            /!*<a onClick={handlesLogout} className="header-btn d-none d-lg-block bg-dark fw-500 text-white font-xsss p-3 ms-auto w100 text-center lh-20 rounded-xl">Login</a>*!/*/}
+                {/*            /!*<a href="/register" className="header-btn d-none d-lg-block bg-current fw-500 text-white font-xsss p-3 ms-2 w100 text-center lh-20 rounded-xl">Register</a>*!/*/}
+                {/*        </div>*/}
+                {/*    </div>*/}
+
                     <div className="row">
-                        <img className="col-xl-5 d-none d-xl-block p-0 vh-90 bg-image-cover bg-no-repeat" alt='happy people' src={baseImage}></img>
-                        <div className="col-xl-7 vh-100 align-items-center d-flex bg-white rounded-3 overflow-hidden">
+                        <img className="col-xl-6 d-none d-xl-block p-0 vh-90 bg-image-cover bg-no-repeat" alt='happy people' src={baseImage}></img>
+                        <div className="col-xl-6 vh-100 align-items-center d-flex bg-white rounded-3 overflow-hidden">
                             <div className="card shadow-none border-0 ms-auto me-auto login-card">
                                 <div className="card-body rounded-0 text-left">
-                                    <h2 className="fw-700 display1-size display2-md-size mb-3">Login into <br />your account</h2>
-                                    <div>
-                                        <a onClick={loginWithRedirect} className="bg-dark fw-500 text-white font-xsss p-3 ms-auto w100 text-center lh-20 rounded-xl">Login</a>
+                                    <img className="mb-5" alt={"Logo"} style={{width: '400px', height: 'auto'}} src={logo}/>
+                                    <h2 className="fw-700 ms-5 display1-size display2-md-size mb-3">Login into <br />your account</h2>
+                                    <div className="ms-5">
+                                        <a onClick={loginWithRedirect} className="bg-success fw-500 text-white font-xsss p-3 ms-auto w100 text-center lh-20 rounded-xl">Login</a>
                                         <a onClick={loginWithRedirect} className="bg-current fw-500 text-white font-xsss p-3 ms-2 w100 text-center lh-20 rounded-xl">Register</a>
                                     </div>
                                 </div>
@@ -91,7 +94,7 @@ const Home = () =>  {
                         </div>
 
                     </div>
-                </div>
+                {/*</div>*/}
             </Fragment>
         )
     }
